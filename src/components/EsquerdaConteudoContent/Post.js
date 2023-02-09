0111b054adc5aacc09df2cf858cac0f13f283b6d
Phebueno <fcbgomes@gmail.com>
2023-02-09 16:15:58 -0300

    Adicionados seletores para autoavaliação

--- src/components/EsquerdaConteudoContent/Post.js
+++ src/components/EsquerdaConteudoContent/Post.js
@@ -12,25 +12,25 @@ export default function Post(props) {
                     </div>
 
                     <div class="conteudo">
-                        <img src={props.imgConteudo} alt={props.imgAltConteudo} />
+                        <img data-test="post-image" src={props.imgConteudo} alt={props.imgAltConteudo} />
                     </div>
 
                     <div class="fundo">
                         <div class="acoes">
                             <div>
-                                <ion-icon name="heart-outline"></ion-icon>
+                                <ion-icon name="heart-outline" data-test="like-post"></ion-icon>
                                 <ion-icon name="chatbubble-outline"></ion-icon>
                                 <ion-icon name="paper-plane-outline"></ion-icon>
                             </div>
                             <div>
-                                <ion-icon name="bookmark-outline"></ion-icon>
+                                <ion-icon name="bookmark-outline" data-test="save-post"></ion-icon>
                             </div>
                         </div>
 
                         <div class="curtidas">
                             <img src="assets/img/respondeai.svg" alt="respondeai" />
                             <div class="texto">
-                                Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
+                                Curtido por <strong>respondeai</strong> e <strong data-test="likes-number">outras 101.523 pessoas</strong>
                             </div>
                         </div>
                     </div>
--- src/components/SidebarContent/Usuario.js
+++ src/components/SidebarContent/Usuario.js
@@ -1,11 +1,11 @@
 export default function Usuario() {
     return (
         <div class="usuario">
-            <img src="assets/img/catanacomics.svg" alt="imagem de perfil" />
+            <img src="assets/img/catanacomics.svg" alt="imagem de perfil" data-test="profile-image"/>
             <div class="texto">
                 <span>
-                    <strong>catanacomics</strong>
-                    <ion-icon name="pencil"></ion-icon>
+                    <strong data-test="name">catanacomics</strong>
+                    <ion-icon name="pencil" data-test="edit-name"></ion-icon>
                 </span>
             </div>
         </div>
